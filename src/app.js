4297a663641d3c37dff5c92122aa5589bc148130
Martin Zauner <zauner.martin1980@gmail.com>
2023-10-26 17:18:53 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log('webserver started');
+    res.send('hello from express');
+})
+
+app.listen(3000);
